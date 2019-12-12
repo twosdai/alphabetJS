27d5a69f13aa207c9a7eca27bbca2ff1b6de9956
Doug Skinner <me@doug-skinner.com>
2019-12-12 12:56:54 -0500

    Initial upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const main = () => {
+    const alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(
+        ","
+    );
+    return alphabet;
+};
+
+module.exports = { main };
